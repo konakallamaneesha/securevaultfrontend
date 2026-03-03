a6574e188a5c7e2a5e24ddfd6950bb254860e17b
konakallamaneesha <konakallamaneesha13@gmail.com>
2026-03-03 11:35:17 +0530

    changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://securevaultbackend-nhyk.onrender.com/api",
+  baseURL: "https://backend-3t2r.onrender.com/api",
 });
 
 export default API;
